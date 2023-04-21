316722f0f6f39d2ec0d2d3b7c1331d3aee453449
white-mice <69317548+white-mice@users.noreply.github.com>
2023-04-21 21:33:59 +0530

    Add files via upload

---
+++ cool_wasm_proj.d.ts
@@ -0,0 +1,25 @@
+declare namespace wasm_bindgen {
+	/* tslint:disable */
+	/* eslint-disable */
+	/**
+	*/
+	export function greet(): void;
+	
+}
+
+declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+declare interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly greet: () => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+declare function wasm_bindgen (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
